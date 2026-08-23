5dd7b6fe2c7cf0e7b62f12578fe131744c5dd5ff
desainer21 <bandawa.lame@gmail.com>
2026-08-23 10:47:44 -0700

    Add Banda Chat PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "banda-chat-v1";
+const APP_SHELL = ["/", "/login", "/manifest.webmanifest", "/icon-192.svg", "/icon-512.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
