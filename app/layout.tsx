dd779eb80c0dcc5a8aca83584d5ec073d338f22f
desainer21 <bandawa.lame@gmail.com>
2026-08-26 03:17:10 -0700

    Add standalone floating notification widget without changing chat pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import GlobalChatFeatures from "@/components/GlobalChatFeatures";
 import PWARegister from "@/components/PWARegister";
+import BandaNotificationWidget from "@/components/BandaNotificationWidget";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,13 +32,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="id"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="id" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         {children}
         <GlobalChatFeatures />
+        <BandaNotificationWidget />
         <PWARegister />
       </body>
     </html>
